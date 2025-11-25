440505dd618dc7c15a6205be942bb45bd578df93
nedru9 <kurshevaadeliya@gmail.com>
2025-11-25 18:57:06 +0800

    Удаление лишних комментов

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use App\Http\Controllers\TokenController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/review/{token}', [TokenController::class, 'show']);
-
-// Маршрут для всех остальных URL
 Route::fallback(function () {
-    abort(404, 'Такая страница не существует'); // Это вызовет страницу 404
+    abort(404, 'Такая страница не существует');
 });
