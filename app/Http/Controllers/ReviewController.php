5c39356454cd6beffa3c290c31dc5f1c8180cdc0
nedru9 <kurshevaadeliya@gmail.com>
2025-11-27 20:26:04 +0800

    Удаление Email сервиса

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -11,7 +11,6 @@ use App\Notifications\ServiceNotificationFactory;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\DB;
 use Throwable;
 
